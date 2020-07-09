ab481f6219e8932f69534b8335ed2ec679706193
Brent Musselman - IT <brent.musselman@rem.remichel.com>
2020-07-09 10:39:28 -0400

    halfway thru 7/9 wrapping up tutorial

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.post_list, name='postlist')
+    path('', views.post_list, name='postlist'),
+    # post/ = means url should begin with post followed by /
+    # <int:pk> an int value and transfers it to view variable called pk
+    # / = finishes url
+    # so /post/num is translated to post number
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- readme.txt
+++ readme.txt
@@ -16,3 +16,4 @@ git stuff
 
 django shell
     python manage.py shell
+    exit()
